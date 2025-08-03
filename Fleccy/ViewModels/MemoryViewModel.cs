b51dbddfe4b16719439d8f83197ab7db03650755
SECT19N <alexmas9n@gmail.com>
2025-08-03 14:32:29 +0300

    Enhance memory data display in MemoryViewModel
    Updated LoadMemoryData to include total and available
    virtual, page file, and physical memory. Improved
    formatting for memory speed display with thousands
    separators for better readability.

--- Fleccy/ViewModels/MemoryViewModel.cs
+++ Fleccy/ViewModels/MemoryViewModel.cs
@@ -13,13 +13,37 @@ public class MemoryViewModel : ViewModelBase {
 	}
 
 	private void LoadMemoryData() {
+		_hardwareInfo.RefreshMemoryStatus();
+
+		// Interesting value here.
+		ulong totalVirtualMem = _hardwareInfo.MemoryStatus.TotalVirtual;
+		ulong availableVirtual = _hardwareInfo.MemoryStatus.AvailableVirtual;
+
+		ulong totalPageFile = _hardwareInfo.MemoryStatus.TotalPageFile;
+		ulong availablePageFile = _hardwareInfo.MemoryStatus.AvailablePageFile;
+
+		ulong totalPhysical = _hardwareInfo.MemoryStatus.TotalPhysical;
+		ulong availablePhysical = _hardwareInfo.MemoryStatus.AvailablePhysical;
+
+		MemoryNodes.Add(new MemoryTreeNode {
+			Name = "Memory Status",
+			Children = [
+				new() { Name = $"Total Virtual Memory: {totalVirtualMem / 1024 / 1024 / 1024:N0} GB" },
+				new() { Name = $"Available Virtual Memory: {availableVirtual / 1024 / 1024 / 1024:N0} GB" },
+				new() { Name = $"Total Page File: {totalPageFile / 1024 / 1024:N0} MB" },
+				new() { Name = $"Available Page File: {availablePageFile / 1024 / 1024:N0} MB" },
+				new() { Name = $"Total Physical Memory: {totalPhysical / 1024 / 1024:N0} MB" },
+				new() { Name = $"Available Physical Memory: {availablePhysical / 1024 / 1024:N0} MB" }
+			]
+		});
+
 		_hardwareInfo.RefreshMemoryList();
 
 		foreach (Memory memory in _hardwareInfo.MemoryList) {
 			MemoryTreeNode memoryNode = new() { Name = $"{memory.BankLabel}" };
 
 			memoryNode.Children.Add(new() { Name = $"Size: {memory.Capacity / 1024 / 1024:N0} MB" });
-			memoryNode.Children.Add(new() { Name = $"Speed: {memory.Speed} MHz" });
+			memoryNode.Children.Add(new() { Name = $"Speed: {memory.Speed:N0} MHz" });
 			memoryNode.Children.Add(new() { Name = $"Manufacturer: {memory.Manufacturer}" });
 			memoryNode.Children.Add(new() { Name = $"Serial Number: {memory.SerialNumber}" });
 			memoryNode.Children.Add(new() { Name = $"Part Number: {memory.PartNumber}" });
